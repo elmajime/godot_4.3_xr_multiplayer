c1305cfb14517a160af49ade9dea80e264f1e4a7
maxime.chambefort <maxime.chambefort@gmail.com>
2024-12-24 14:54:28 +0100

    Synchronsation of players proxies work on simulator

--- level1.gd
+++ level1.gd
@@ -2,10 +2,15 @@ extends XRToolsSceneBase
 
 @export var player_scene : PackedScene
 
+
+func _enter_tree():
+	#set_multiplayer_authority(int(str(name)))
+	$MultiplayerSpawner.spawn_function = spawn_player
+	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Lobby.player_loaded.rpc_id(1) # Tell the server that this peer has loaded.
-	$MultiplayerSpawner.spawned.connect(_on_player_added)
+	#$MultiplayerSpawner.spawned.connect(_on_player_added)
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,31 +24,48 @@ func start_game():
 	print("start_game + nb players : " + str(Lobby.players.size()))
 	for id in Lobby.players:
 		print("start_game " + str(id))
-		instantiate_player(id)
+		#instantiate_player(id)
+		#init_proxy_local_proxy.rpc_id(id)
+		$MultiplayerSpawner.spawn(id)
 		
-	init_proxy_local_proxy()
-	
+		
+	#init_proxy_local_proxy.rpc()
+	#var player: Node3D = $MultiplayerSpawner/Players.find_child(str(multiplayer.get_unique_id()))
+	#player.xr_origin_node = get_tree().root.find_child("XROrigin3D")
 
-func instantiate_player(id):
+func spawn_player(id):
+	print("spawning " + str(id)  + " from " + str(multiplayer.get_unique_id()))
 	var player = player_scene.instantiate()
 	player.name = str(id)
-	$MultiplayerSpawner/Players.add_child(player)
-	player.position = $MarkerPlayer1.position if id == 1 else $MarkerPlayer2.position
+	var offset = $MarkerPlayer1.transform if id == 1 else $MarkerPlayer2.transform
+	player.offset = offset
 	
-func _on_player_added(node):
-	print("_on_player_added from " + str(multiplayer.get_unique_id()))
-	print(node.name)
-	init_proxy_local_proxy.rpc_id(multiplayer.get_unique_id())
-	
-@rpc("call_local")
-func init_proxy_local_proxy():
-	var id = multiplayer.get_unique_id()
-	print("init_proxy_local_proxy from " + str(id))
-	print("init_proxy_local_proxy nb players : " + str($MultiplayerSpawner/Players.get_child_count()))
-	for p in $MultiplayerSpawner/Players.get_children():
-		print("init_proxy_local_proxy" + " " + p.name)
+	if id == multiplayer.get_unique_id():
+		print("setting xr_origin_node")
+		var xr_origin_node = get_node("XROrigin3D")
+		player.xr_origin_node = xr_origin_node
 		
-	var player: Node3D = $MultiplayerSpawner/Players.find_child(str(id))
-	if player:
-		# we only do this if the proxy related to our local instance is spawned
-		player.xr_origin_node = get_tree().root.find_child("XROrigin3D")
+	return player
+
+#func instantiate_player(id):
+	#var player = player_scene.instantiate()
+	#player.name = str(id)
+	#$MultiplayerSpawner/Players.add_child(player)
+	#player.position = $MarkerPlayer1.position if id == 1 else $MarkerPlayer2.position
+	
+#func _on_player_added(node):
+	#print("_on_player_added from " + str(multiplayer.get_unique_id()))
+	#print(node.name)
+	###init_proxy_local_proxy.rpc_id(multiplayer.get_unique_id())
+	##init_proxy_local_proxy()
+	#pass
+	
+#@rpc("call_local")
+#func init_proxy_local_proxy():
+	#var id = multiplayer.get_unique_id()
+	#print("init_proxy_local_proxy from " + str(id))
+		#
+	#var player: Node3D = $MultiplayerSpawner/Players.find_child(str(id))
+	#if player:
+		## we only do this if the proxy related to our local instance is spawned
+		#player.xr_origin_node = get_tree().root.find_child("XROrigin3D")
--- player_proxy.gd
+++ player_proxy.gd
@@ -1,24 +1,26 @@
 extends Node3D
 
 @export var xr_origin_node: Node3D
+@export var offset: Transform3D
+
 var xr_head : Node3D
 var xr_left_hand : Node3D
 var xr_right_hand : Node3D
 
 func _enter_tree():
 	set_multiplayer_authority(int(str(name)))
-	
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if is_multiplayer_authority() and xr_origin_node:
-		xr_head = xr_origin_node.find_child("XRCamera3D")
-		xr_left_hand = xr_origin_node.find_child("LeftHand")
-		xr_right_hand = xr_origin_node.find_child("RightHand")
+	if xr_origin_node:
+		xr_head = xr_origin_node.get_node("XRCamera3D")
+		xr_left_hand = xr_origin_node.get_node("LeftHand")
+		xr_right_hand = xr_origin_node.get_node("RightHand")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if is_multiplayer_authority() and xr_origin_node:
-		$Head.transform = xr_head.transform
-		$Head/LeftHand.transform = xr_left_hand.transform
-		$Head/RightHand.transform = xr_right_hand.transform
+	if xr_origin_node:			
+		$Head.transform = offset * xr_head.transform
+		$LeftHand.transform = offset * xr_left_hand.transform
+		$RightHand.transform = offset * xr_right_hand.transform
